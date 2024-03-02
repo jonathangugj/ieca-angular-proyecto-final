f84fd1677e49ea031b93954f6c0ebb87955735eb
Jonathan Adrian Guadian Granados <jonathan.gugj@gmail.com>
2024-03-01 19:59:30 -0600

    Se elimina framewok angular materia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,9 @@ import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { NavbarComponent } from './navbar/navbar.component';
 
-import {MatIconModule} from '@angular/material/icon';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-
 import { HomeComponent } from './home/home.component';
 import { BuscaComponent } from './busca/busca.component';
 import { FavoritosComponent } from './favoritos/favoritos.component';
-import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
@@ -27,11 +21,6 @@ import {MatGridListModule} from '@angular/material/grid-list';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatIconModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatCardModule,
-    MatGridListModule,
   ],
   providers: [
     provideClientHydration(),
